4974e2a40d6279a5c8a5edfed828e52946f72c10
totoargumedo <totoargumedo@gmail.com>
2026-09-01 13:28:47 -0300

    Sumamos objetos y arrays

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
+let pepe = { nombre: "Pepe", edad: 30, ciudad: "Madrid", carnet: true };
+
+const monica = [1, 2, 3, 4, 5];
+
+console.log(pepe);
+console.log(monica);
+
+// console.log(monica[3]);
+
+monica[3] = "Tito";
+pepe.edad = 38;
+
+console.log(pepe);
+console.log(monica);
+
+// console.log(monica[3]);
 // let numero_usuario = prompt("Ingresar un numero entero");
 
 // console.log("El numero ingresado es: " + numero_usuario);

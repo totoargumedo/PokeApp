7a71276122f20177f6dbad0d747092bb9e77f187
totoargumedo <totoargumedo@gmail.com>
2026-09-01 13:05:46 -0300

    Practica basica tipos de datos

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// let numero_usuario = prompt("Ingresar un numero entero");
+
+// console.log("El numero ingresado es: " + numero_usuario);
+
+// let numero_usuario2 = parseInt(numero_usuario);
+
+// console.log("El numero ingresado es: ");
+// console.log(numero_usuario2);
+
+// let variable1 = "22";
+// let variable2 = 22;
+
+// console.log(variable1);
+// console.log(variable2);
+
+// if (variable1 == variable2) {
+//   console.log("Son iguales");
+// } else {
+//   console.log("No son iguales");
+// }
+
+// variable1 = 22;
+
+// if (variable1 === variable2) {
+//   console.log("Son iguales");
+// } else {
+//   console.log("No son iguales");
+// }
